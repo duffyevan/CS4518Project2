79d999a5d7faf22831d4835418b1db399b69e9e7
Evan Duffy <elduffy@wpi.edu>
2018-11-05 23:43:24 -0500

    Added basic picture taking. (Finished Step 5)

--- app/src/main/java/com/evan/cameratutorial/MainActivity.java
+++ app/src/main/java/com/evan/cameratutorial/MainActivity.java
@@ -1,13 +1,37 @@
 package com.evan.cameratutorial;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected static int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        List<ResolveInfo> activities = getPackageManager().queryIntentActivities(takePictureIntent,0);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_IMAGE_CAPTURE)
+            return;
+        ImageView image = findViewById(R.id.imageView);
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        image.setImageBitmap(thumbnail);
     }
 }
